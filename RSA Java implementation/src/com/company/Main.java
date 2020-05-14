1e6713e7097e48ee5f432cdf328c3b1085c3311b
keserukristof <41549423+keserukristof@users.noreply.github.com>
2020-05-14 13:53:01 +0200

    add comments

--- RSA Java implementation/src/com/company/Main.java
+++ RSA Java implementation/src/com/company/Main.java
@@ -12,16 +12,20 @@ public class Main {
         BigInteger p, q, n, fin, e, d, m;
 
         System.out.println("Adj meg egy nagy prím számot!");
-        p = sc.nextBigInteger(); //p prím
+        p = sc.nextBigInteger(); // p prím
 
         System.out.println("Adj meg mégegy nagy prím számot!");
-        q = sc.nextBigInteger(); //q prím
+        q = sc.nextBigInteger(); // q prím
 
         n = p.multiply(q); // n = p * q
 
         fin = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE)); // f(n) = (p - 1) * (q - 1)
 
+        //az e kezdő értékének 3-at adok meg
         e = BigInteger.valueOf(3);
+        //egy while ciklus addig fog 2-t hozzáadni a 3-hoz
+        //amíg nem lesz igaz, hogy a f(n) és az e
+        //legnagyobb közös osztója 1
         while (!fin.gcd(e).equals(BigInteger.ONE)) {
             e = e.add(BigInteger.valueOf(2));
         }
@@ -39,15 +43,18 @@ public class Main {
         System.out.println("Privát kulcs: " + d);
 
         System.out.println("Az üzenet: " + m);
-        m = m.modPow(e, n);
+
+        m = m.modPow(e, n); //titkosítás: m^e (mod n)
         System.out.println("Kodolt üzente: " + m);
-        m = m.modPow(d, n);
+
+        //itt az m már a titkosított üzenet
+        m = m.modPow(d, n); //visszafejtés: m^d
         System.out.println("Dekódolt üzenet: " + m);
     }
 
     public static BigInteger getTheD(BigInteger e, BigInteger fin) {
         BigInteger d;
-        d = e.modInverse(fin); //e^(-1)(mod(fi(n))
+        d = e.modInverse(fin); //d az inverze az e(mod f(n))-nek így d = e^(-1)(mod(fi(n))
         return d;
     }
 }
